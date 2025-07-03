c7aa0a9cc6302426636cdc9ef3b4a29a3a230381
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-07-03 13:13:51 +0100

    add memory to agent

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,6 +1,15 @@
 import { anthropic } from "@ai-sdk/anthropic";
 import { Agent } from "@mastra/core/agent";
 import { weatherTool } from "../tools";
+import { Memory } from "@mastra/memory";
+import { LibSQLStore } from "@mastra/libsql";
+
+const memory = new Memory({
+  storage: new LibSQLStore({
+    url: process.env.TURSO_URL!,
+    authToken: process.env.TURSO_TOKEN!
+  })
+});
 
 export const weatherAgent = new Agent({
   name: "Weather Agent",
@@ -17,5 +26,6 @@ export const weatherAgent = new Agent({
       Use the weatherTool to fetch current weather data.
 `,
   model: anthropic("claude-3-5-sonnet-20240620"),
-  tools: { weatherTool }
+  tools: { weatherTool },
+  memory
 });
